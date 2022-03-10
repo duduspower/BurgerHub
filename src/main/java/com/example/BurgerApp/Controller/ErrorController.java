8db1721058719c260bb28053ce20f5c3e0b5b274
Dominik Duda <dominikduda191@gmail.com>
2022-03-10 16:19:03 +0100

    Adding Error page

--- src/main/java/com/example/BurgerApp/Controller/ErrorController.java
+++ src/main/java/com/example/BurgerApp/Controller/ErrorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/error")
 public class ErrorController {
 
-
+    @GetMapping
     public String displayErrorPage(){
         return "errorPage";
     }
